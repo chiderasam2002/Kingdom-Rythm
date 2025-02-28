34366cefad1b9ce23af4cdf43baf39ae55c88743
chiderasam2002 <165075524+chiderasam2002@users.noreply.github.com>
2025-02-28 21:01:51 +0100

    kingdom rythm

---
+++ js/index.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function showHeader(){
+    const header = document.querySelector('header')
+    header.style.display = 'flex'
+}
+
+function hideHeader(){
+    const header = document.querySelector('header')
+    header.style.display = 'none'
+}
+
+
+
+
+
+
+
+// function myFunction() {
+//     var x = document.getElementById("topnav");
+//     if (x.className === "nav") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "topnav";
+//     }
+//   }
+  
